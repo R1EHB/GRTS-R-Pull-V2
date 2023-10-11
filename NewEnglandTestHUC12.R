13f18fbc8cd2aded645841dce607f88f6f2891f4
Erik Beck <beck.erik@epa.gov>
2023-10-11 18:11:51 -0400

    need to functionize

--- NewEnglandTestHUC12.R
+++ NewEnglandTestHUC12.R
@@ -10,14 +10,22 @@ library(curl)
 library(tidyjson)
 library(tidyr)
 
-BaseURL <- "https://ordspub.epa.gov/ords/grts_rest/grts_rest_apex/grts_rest_apex/GetProjectsByHUC12/"
+BaseURL <-
+  "https://ordspub.epa.gov/ords/grts_rest/grts_rest_apex/grts_rest_apex/GetProjectsByHUC12/"
 
 
 # Get the new england HUC12 list
 
+# need to simplify flow logic with using a subroutine/function(s)
 
+# move to function?
 
-myHUC12Vector <- read.csv(file='HUC12NewEnglandvector.csv' , header = T, colClasses = c("HUC12_Code"="character"))
+myHUC12Vector <-
+  read.csv(
+    file = 'HUC12NewEnglandvector.csv' ,
+    header = T,
+    colClasses = c("HUC12_Code" = "character")
+  )
 
 
 head (myHUC12Vector)
@@ -45,156 +53,164 @@ vec_len = 10
 
 # Create a progress bar for the loop
 
-pb = txtProgressBar(min = 0, max = vec_len, initial = 0) 
+pb = txtProgressBar(min = 0, max = vec_len, initial = 0)
 
 # Setup first element
- URL <- paste0(BaseURL, myHUC12Vector$HUC12_Code[1])
-  
-  DataRequestRaw <- curl_fetch_memory(URL)
-  DataRequest <- rawToChar(DataRequestRaw$content)
+URL <- paste0(BaseURL, myHUC12Vector$HUC12_Code[1])
+
+DataRequestRaw <- curl_fetch_memory(URL)
+DataRequest <- rawToChar(DataRequestRaw$content)
 #  str (DataRequest)
 
-  # Now start several steps to disentangle the nested data from the https fetch operation #
-  DataRequest %>% spread_all -> GRTSInfo4HUC
+# Now start several steps to disentangle the nested data from the https fetch operation #
+DataRequest %>% spread_all -> GRTSInfo4HUC
 
- # Now loop
+# Now loop
 for (i in 2:vec_len) {
   URL <- paste0(BaseURL, myHUC12Vector$HUC12_Code[i])
   
   DataRequestRaw <- curl_fetch_memory(URL)
   DataRequest <- rawToChar(DataRequestRaw$content)
-#  str (DataRequest)
-
+  #  str (DataRequest)
+  
+  # move to function here?
+  
   # Now start several steps to disentangle the nested data from the https fetch operation #
   DataRequest %>% spread_all -> tempGRTSInfo4HUC
- GRTSInfo4HUC <- bind_rows (GRTSInfo4HUC, tempGRTSInfo4HUC)
-}
-
+  GRTSInfo4HUC <- bind_rows (GRTSInfo4HUC, tempGRTSInfo4HUC)
+  
+  
+  #  str(GRTSInfo4HUC)
+  
+  
+  ## Grab the metadata ##
+  document.id <- GRTSInfo4HUC$document.id
+  hasMore <-
+    GRTSInfo4HUC$hasMore # Are more top-level records for this HUC?
+  limit <- GRTSInfo4HUC$limit # Limit of projects listed per HUC?
+  offset <- GRTSInfo4HUC$offset # Not sure what offset is
+  count <- GRTSInfo4HUC$count  # How many projects are in this HUC
+  
+  
+  HUCMetaData <- data.frame (document.id, hasMore, limit, offset, count)
+  
+  
+  # Now start several steps to disentangle the nested data from the https fetch operation #
+  
+  
+  DataRequest %>% spread_all -> GRTSInfo4HUC
   str(GRTSInfo4HUC)
   
-
-## Grab the metadata ##
-document.id <- GRTSInfo4HUC$document.id
-hasMore <- GRTSInfo4HUC$hasMore # Are more top-level records for this HUC?
-limit <- GRTSInfo4HUC$limit # Limit of projects listed per HUC?
-offset <- GRTSInfo4HUC$offset # Not sure what offset is
-count <- GRTSInfo4HUC$count  # How many projects are in this HUC
-
-
-HUCMetaData <- data.frame (document.id,hasMore,limit,offset,count)
-
-
-# Now start several steps to disentangle the nested data from the https fetch operation #
-
-
-DataRequest %>% spread_all -> GRTSInfo4HUC
-str(GRTSInfo4HUC)
-
-## Grab the metadata ##
-document.id <- GRTSInfo4HUC$document.id
-hasMore <- GRTSInfo4HUC$hasMore # Are more top-level records for this HUC?
-limit <- GRTSInfo4HUC$limit # Limit of projects listed per HUC?
-offset <- GRTSInfo4HUC$offset # Not sure what offset is
-count <- GRTSInfo4HUC$count  # How many projects are in this HUC
-
-
-HUCMetaData <- data.frame (document.id,hasMore,limit,offset,count)
-
-
-# print (HUCMetaData)
-
-
-## Clear these for reuse ##
-
-rm (document.id, hasMore, limit, offset, count)
-
-
-# Isolate the main data #
-
-
-GRTSInfo4HUC$..JSON  -> HUCsDetails
-
- str (HUCsDetails)
- print (HUCsDetails)
-
-
-
-ProjsInHUC.OuterList <- HUCsDetails[[1]] # One or more projects per HUC (in this URL fetch)
-
-str (ProjsInHUC.OuterList ) # This is a nested list of all the projects in the huc.  Need to loop over each
-
-
-
-## Gather up the Project-level metadata ##
-
-hasMore <- ProjsInHUC.OuterList$hasMore
-limit <- ProjsInHUC.OuterList$limit
-offset <- ProjsInHUC.OuterList$offset
-count  <- ProjsInHUC.OuterList$count
-
-ProjMetaData <- data.frame (hasMore,limit,offset,count)
-ProjLinks <- (ProjsInHUC.OuterList$links)
-
-# str (ProjLinks)
-
-## Clear these for reuse ##
-
-rm (hasMore, limit, offset, count)
-
-# str(ProjsInHUC.OuterList)
-# print (ProjsInHUC.OuterList)
-
-# It seems ProjMetaData and HUCMetaData elements should and do match, but watch for this later
-print (ProjMetaData)
-print (HUCMetaData)
-
-# Loop over the elements of ProjsInHUC (i), from one to j
-# j is count of projects from HUCMetaData or ProjMetaData
-
-i=1
-j <- HUCMetaData$count
-
-
-# Create Data Frame with first row of data
-
-# Note to self: delete somewhere the href...X and rel...X columns
-
-ProjDetailList <-ProjsInHUC.OuterList[[1]][[i]]
-temp_df <- do.call(cbind, ProjDetailList)
-temp_frame <- as.data.frame(temp_df)
-ProjDetailFrame  <- bind_cols (temp_frame, ProjMetaData, ProjLinks)
-
-str (ProjDetailFrame)
+  ## Grab the metadata ##
+  document.id <- GRTSInfo4HUC$document.id
+  hasMore <-
+    GRTSInfo4HUC$hasMore # Are more top-level records for this HUC?
+  limit <- GRTSInfo4HUC$limit # Limit of projects listed per HUC?
+  offset <- GRTSInfo4HUC$offset # Not sure what offset is
+  count <- GRTSInfo4HUC$count  # How many projects are in this HUC
   
-for (i in 2:j) {
-  print (i)
-  ProjDetailList <-ProjsInHUC.OuterList[[1]][[i]]
+  
+  HUCMetaData <- data.frame (document.id, hasMore, limit, offset, count)
+  
+  
+  # print (HUCMetaData)
+  
+  
+  ## Clear these for reuse ##
+  
+  rm (document.id, hasMore, limit, offset, count)
+  
+  
+  # Isolate the main data #
+  
+  
+  GRTSInfo4HUC$..JSON  -> HUCsDetails
+  
+  str (HUCsDetails)
+  print (HUCsDetails)
+  
+  
+  
+  ProjsInHUC.OuterList <-
+    HUCsDetails[[1]] # One or more projects per HUC (in this URL fetch)
+  
+  str (ProjsInHUC.OuterList) # This is a nested list of all the projects in the huc.  Need to loop over each
+  
+  
+  
+  ## Gather up the Project-level metadata ##
+  
+  hasMore <- ProjsInHUC.OuterList$hasMore
+  limit <- ProjsInHUC.OuterList$limit
+  offset <- ProjsInHUC.OuterList$offset
+  count  <- ProjsInHUC.OuterList$count
+  
+  ProjMetaData <- data.frame (hasMore, limit, offset, count)
+  ProjLinks <- (ProjsInHUC.OuterList$links)
+  
+  # str (ProjLinks)
+  
+  ## Clear these for reuse ##
+  
+  rm (hasMore, limit, offset, count)
+  
+  # str(ProjsInHUC.OuterList)
+  # print (ProjsInHUC.OuterList)
+  
+  # It seems ProjMetaData and HUCMetaData elements should and do match, but watch for this later
+  print (ProjMetaData)
+  print (HUCMetaData)
+  
+  # Loop over the elements of ProjsInHUC (i), from one to j
+  # j is count of projects from HUCMetaData or ProjMetaData
+  
+  i = 1
+  j <- HUCMetaData$count
+  
+  
+  # Create Data Frame with first row of data
+  
+  # Note to self: delete somewhere the href...X and rel...X columns
+  
+  ProjDetailList <- ProjsInHUC.OuterList[[1]][[i]]
   temp_df <- do.call(cbind, ProjDetailList)
   temp_frame <- as.data.frame(temp_df)
-  temp_frame  <- bind_cols (temp_frame, ProjMetaData, ProjLinks)
- ProjDetailFrame <- bind_rows(ProjDetailFrame,temp_frame)
-
+  ProjDetailFrame  <- bind_cols (temp_frame, ProjMetaData, ProjLinks)
+  
+  str (ProjDetailFrame)
+  
+  # test to see if j is > 1.  Only do this loop if j > 1
+  # also need to test the edge case of j=0
+  
+  if (j > 1) {
+    for (i in 2:j) {
+      print (i)
+      ProjDetailList <- ProjsInHUC.OuterList[[1]][[i]]
+      temp_df <- do.call(cbind, ProjDetailList)
+      temp_frame <- as.data.frame(temp_df)
+      temp_frame  <- bind_cols (temp_frame, ProjMetaData, ProjLinks)
+      ProjDetailFrame <- bind_rows(ProjDetailFrame, temp_frame)
+    }
+  }
+  
+  str (ProjDetailFrame)
+  
+  # Keep only the variables we want
+  
+  # drop href variables
+  VarNames <- colnames(ProjDetailFrame)
+  href_drop <- grepl (pattern = 'href...', VarNames)
+  ProjDetailFrame <- ProjDetailFrame[!href_drop]
+  
+  
+  # drop rel variables
+  
+  VarNames <- colnames(ProjDetailFrame)
+  rel_drop  <- grepl (pattern = 'rel...' , VarNames)
+  ProjDetailFrame <- ProjDetailFrame[!rel_drop]
+  
+  # Drop from data frame those columns with href or rel
 }
-
-str (ProjDetailFrame)
-
-# Keep only the variables we want
-
-# drop href variables
-VarNames <- colnames(ProjDetailFrame)
-href_drop <- grepl (pattern='href...',VarNames)
-ProjDetailFrame <- ProjDetailFrame[!href_drop]
-
-
-# drop rel variables
-
-VarNames <- colnames(ProjDetailFrame)
-rel_drop  <- grepl (pattern='rel...' ,VarNames)
-ProjDetailFrame <- ProjDetailFrame[!rel_drop]
-
-# Drop from data frame those columns with href or rel
-
-write.csv(ProjDetailFrame, file="test.csv")
+write.csv(ProjDetailFrame, file = "test.csv")
 
 q()
-
