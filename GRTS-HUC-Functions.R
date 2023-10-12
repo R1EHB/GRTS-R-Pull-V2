579164bb0eba4815a6b8d14d348caecd2234a1fa
Erik Beck <erikbeck@WahpenayoPeak.tahoma.link>
2023-10-12 12:22:44 -0400

    working on test, cleaning up old files

--- GRTS-HUC-Functions.R
+++ GRTS-HUC-Functions.R
@@ -1,3 +1,26 @@
 #
 ################  Functions for Shrimp-n-GRTS R Code ###############
 
+####  Vector of HUCs to Analyze #####
+
+# To be added. Maybe #
+
+#####################################
+
+
+########  Grab a record from the API and return it ####
+
+## Function takes in the URL to grab, returns the content portion in a tibble
+
+HUCDataContent <- function (URL="https://grts.epa.gov") {
+    DataRequestRaw <- curl_fetch_memory(URL)
+    DataRequest <- rawToChar(DataRequestRaw$content)
+
+   # Spread the JSON Data
+    DataRequest %>% spread_all -> GRTSInfo4HUC
+   # Return GRTSInfo4HUC
+    return (GRTSInfo4HUC)
+}
+
+
+##########################
---
+++ Test-GRTS-Functions.R
@@ -0,0 +1,60 @@
+# Test the GRTS Functions #
+
+
+
+## Load Libraries ##
+library(httr)
+library(jsonlite)
+library(data.table)
+library(dplyr)
+library(curl)
+library(tidyjson)
+library(tidyr)
+
+
+
+# Test the URL Fetch and spread #
+
+source ("GRTS-HUC-Functions.R")
+
+BaseURL <-
+  "https://ordspub.epa.gov/ords/grts_rest/grts_rest_apex/grts_rest_apex/GetProjectsByHUC12/"
+
+
+# Get the new england HUC12 list
+
+# need to simplify flow logic with using a subroutine/function(s)
+
+# move to function?
+
+myHUC12Vector <-
+  read.csv(
+    file = 'HUC12NewEnglandvector.csv' ,
+    header = T,
+    colClasses = c("HUC12_Code" = "character")
+  )
+
+
+head (myHUC12Vector)
+str (myHUC12Vector)
+
+# vec_len <- length (myHUC12Vector)
+
+# Temporarily put vec_len as 10 for testing
+
+vec_len = 10
+
+# Setup first element
+URL <- paste0(BaseURL, myHUC12Vector$HUC12_Code[1])
+
+HUC_Content <- HUCDataContent(URL)
+
+for (i in 2:vec_len) {
+    URL <- paste0(BaseURL, myHUC12Vector$HUC12_Code[i])
+    tempHUC_Content[i] <-   HUCDataContent (URL)
+    bind_rows (HUC_Content, tempHUC_Content)
+}
+
+str (HUC_Content)
+
+q()
