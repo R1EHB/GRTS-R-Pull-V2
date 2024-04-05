47675456d034a8e5d880dfcb4676742db432ea22
Erik Beck <bacon@tahoma.com>
2024-04-05 13:00:09 -0400

    updating url

--- RI-HUC12-get.R
+++ RI-HUC12-get.R
@@ -2,16 +2,32 @@
 # Using for Load Listings 2024
 # 4 April 2024
 
+# Note: for openSSL 3.0.2 on linux, need to do:
+
+#You could also just add Options = UnsafeLegacyServerConnect to the
+#  existing /etc/ssl/openssl.cnf under [system_default_sect].
+
+#NB. In OpenSSL < 3.0.4 there was a bug that ignored the
+# UnsafeLegacyServerConnect option. If you are stuck with <= 3.0.3, you
+# could use (the more unsafe) UnsafeLegacyRenegotiation instead.
+
+# https://stackoverflow.com/questions/75763525/curl-35-error0a000152ssl-routinesunsafe-legacy-renegotiation-disabled
+
 
 ## Set BaseURL ##
-BaseURL <- "https://ofmpub.epa.gov/apex/grts_rest/GetProjectsByHUC12/"
 
+# Old BaseURL API Endpoint
+#BaseURL <- "https://ofmpub.epa.gov/apex/grts_rest/GetProjectsByHUC12/"
+
+# New URL (per matt moss, 5 april 2024)
+BaseURL <- "https://ordspub.epa.gov/ords/grts_rest/grts_rest_apex/grts_rest_apex/GetProjectsByHUC12/"
 
 # Get the RI HUC12 list
 
 
 
-myRIHUC12Vector <- read.csv(file="../HUC-Data-Lists/RI-huc.csv",header = T, colClasses = c("HUC12_Code"="character"))
+# myRIHUC12Vector <- read.csv(file="../HUC-Data-Lists/RI-huc.csv",header = T,sep="\t", colClasses = c("HUC12_Code"="character"))
+myRIHUC12Vector <- read.csv(file="../HUC-Data-Lists/RI-huc.csv",header = T,sep="\t")
 
 head (myRIHUC12Vector)
 str (myRIHUC12Vector)
@@ -51,11 +67,11 @@ library(readr)
 # items.watershed_plans (chr) 
 
 
-vec_len <- length (myRIHUC12Vector)
+# vec_len <- length (myRIHUC12Vector)
 
 # Temporarily put vec_len as 10 for testing
 
-# vec_len = 10
+ vec_len = 10
 
 
 
@@ -76,7 +92,7 @@ vec_len <- length (myRIHUC12Vector)
 pb = txtProgressBar(min = 0, max = vec_len, initial = 0) 
 
 for (i in 1:vec_len) {
-    URL <- paste0(BaseURL, myRIHUC12Vector$HUC12_Code[i])
+    URL <- paste0(BaseURL, myRIHUC12Vector$huc12[i])
 
     raw_data <- httr::GET (URL)
     temp <- fromJSON(rawToChar(raw_data$content), flatten = TRUE)
@@ -92,7 +108,7 @@ for (i in 1:vec_len) {
 close(pb)
 
 str(coarseGRTS)
-
+ q()
 
 ## now split and combine the list of dataframes into one dataframe.
 ## It will yield a mix of API URL interspersed with the data of interest retreived from that API URL call.
